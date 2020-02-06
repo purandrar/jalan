0e4f1446b89c72c3260b553bf9696410d0153de5
vindobangun8 <vindobangun8@gmail.com>
2020-02-06 20:18:02 +0700

    tambah asosiasi dan column userid

--- server/models/event.js
+++ server/models/event.js
@@ -6,10 +6,12 @@ module.exports = (sequelize, DataTypes) => {
     name: DataTypes.STRING,
     date: DataTypes.DATE,
     description: DataTypes.STRING,
-    location: DataTypes.STRING
+    location: DataTypes.STRING,
+    UserId:DataTypes.INTEGER
   },sequelize)
   Event.associate = function(models) {
     // associations can be defined here
+    Event.belongsTo(models.User)
   };
   return Event;
 };
